bd30f89cddfb5a1dfd0042435824650e72f5445d
Alina Tarasova <alishka.tarasova@gmail.com>
2023-12-01 15:16:48 +0300

    Added JsonFormatter.java

--- app/src/main/java/hexlet/code/formatters/JsonFormatter.java
+++ app/src/main/java/hexlet/code/formatters/JsonFormatter.java
@@ -2,11 +2,8 @@ package hexlet.code.formatters;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 
-import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
-import java.util.stream.Collectors;
 
 public class JsonFormatter {
 

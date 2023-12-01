09475126294f660e0bffb94784eed36a9ebb67de
Alina Tarasova <alishka.tarasova@gmail.com>
2023-12-01 13:46:12 +0300

    Updated Differ.java Parser.java

--- app/src/main/java/hexlet/code/Differ.java
+++ app/src/main/java/hexlet/code/Differ.java
@@ -3,22 +3,28 @@ package hexlet.code;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.LinkedHashMap;
+import java.util.List;
 import java.util.Map;
-import java.util.Set;
-import java.util.TreeSet;
-
 
 public class Differ {
-    public static String generate(String filepath1, String filepath2) throws Exception {
+    public static String generate(String filepath1, String filepath2, String stylish) throws Exception {
         //формирую мапы с ключами-значениями:
         Map<String, Object> preparedDataFromFile1 = getData(filepath1);
         Map<String, Object> preparedDataFromFile2 = getData(filepath2);
 
         //словарь с изменениями:
-        Map<String, Object> changes = genDiff(preparedDataFromFile1, preparedDataFromFile2);
+        List<Map<String, Object>> differences = Comparator.genDiff(preparedDataFromFile1, preparedDataFromFile2);
 
-        return genToString(changes);
+        switch (stylish) {
+            case "json":
+                return Formatter.generateJson(differences);
+            case "stylish":
+                return Formatter.generateStylish(differences);
+            case "plain":
+                return Formatter.generatePlain(differences);
+            default:
+                throw new Exception("Unknown format: '" + stylish + "'");
+        }
     }
 
     private static Map<String, Object> getData(String filePath) throws Exception {
@@ -47,45 +53,5 @@ public class Differ {
         return index > 0
                 ? filePath.substring(index + 1)
                 : "";
-    }
-
-    public static Map<String, Object> genDiff(Map<String, Object> data1, Map<String, Object> data2) {
-        Map<String, Object> result = new LinkedHashMap<>();
-        Set<String> keys = new TreeSet<>(data1.keySet());
-        keys.addAll(data2.keySet());
-
-        for (String key : keys) {
-            if (!data1.containsKey(key)) {
-                var addedValue = data2.get(key);
-                String addedKey = "  + " + key;
-                result.put(addedKey, addedValue);
-            } else if (!data2.containsKey(key)) {
-                var deletedValue = data1.get(key);
-                String deletedKey = "  - " + key;
-                result.put(deletedKey, deletedValue);
-            } else if (data1.containsKey(key) && data2.containsKey(key)) {
-                if (!data1.get(key).equals(data2.get(key))) {
-                    String changedKey1 = "  - " + key;
-                    String changedKey2 = "  + " + key;
-                    result.put(changedKey1, data1.get(key));
-                    result.put(changedKey2, data2.get(key));
-                } else {
-                    String sameKey = "    " + key;
-                    result.put(sameKey, data1.get(key));
-                }
-            }
-        }
-        return result;
-    }
-
-    public static String genToString(Map<String, Object> diffData) {
-
-        var result = new StringBuilder("{\n");
-
-        for (Map.Entry<String, Object> entry : diffData.entrySet()) {
-            result.append(entry.getKey() + ": " + entry.getValue());
-            result.append("\n");
-        }
-        return result.append("}").toString();
     }
 }

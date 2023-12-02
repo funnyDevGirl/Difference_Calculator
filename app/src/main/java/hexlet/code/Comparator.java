17364025f81e863ef6b97a861352e4bc6ee6b8cf
Alina Tarasova <alishka.tarasova@gmail.com>
2023-12-02 21:27:45 +0300

    Updated Comparator.java

--- app/src/main/java/hexlet/code/Comparator.java
+++ app/src/main/java/hexlet/code/Comparator.java
@@ -1,11 +1,13 @@
 package hexlet.code;
 
 import java.util.Map;
+import java.util.TreeMap;
 import java.util.List;
 import java.util.ArrayList;
 import java.util.Set;
 import java.util.TreeSet;
 import java.util.Objects;
+
 
 public class Comparator {
     public static List<Map<String, Object>> genDiff(Map<String, Object> data1, Map<String, Object> data2)
@@ -19,16 +21,21 @@ public class Comparator {
             //есть в 1, нет во 2:
             if (!data2.containsKey(key)) {
                 Object value1 = !Objects.isNull(data1.get(key)) ? data1.get(key) : "null";
-                Map<String, Object>  removedValues = Map.of("name", key, "type", "deleted",
+                Map<String, Object> removedValues = Map.of("name", key, "type", "deleted",
                         "oldValue", value1);
-                result.add(removedValues);
+                Map<String, Object> removed = new TreeMap<>(removedValues);
+
+                result.add(removed);
 
                 //нет в 1, есть во 2:
             } else if (!data1.containsKey(key)) {
                 Object value2 = !Objects.isNull(data2.get(key)) ? data2.get(key) : "null";
+
                 Map<String, Object> addedValues = Map.of("name", key, "type", "added",
                         "newValue", value2);
-                result.add(addedValues);
+                Map<String, Object> added = new TreeMap<>(addedValues);
+
+                result.add(added);
 
                 //есть в обоих:
             } else if (data1.containsKey(key) && data2.containsKey(key)) {
@@ -39,25 +46,19 @@ public class Comparator {
                 if (!valueForKey1.equals(valueForKey2)) {
                     Map<String, Object> changedValues = Map.of("name", key, "type", "changed",
                             "oldValue", valueForKey1, "newValue", valueForKey2);
-                    result.add(changedValues);
+                    Map<String, Object> changed = new TreeMap<>(changedValues);
 
+                    result.add(changed);
                     //значение не изменилось:
                 } else {
                     Map<String, Object> unchangedValues = Map.of("name", key, "type", "unchanged",
                             "oldValue", valueForKey1);
-                    result.add(unchangedValues);
+                    Map<String, Object> unchanged = new TreeMap<>(unchangedValues);
+
+                    result.add(unchanged);
                 }
             }
         }
         return result;
-    }
-
-    private static String stringify(Object value) {
-
-        if (value == null) {
-            return "null";
-        }
-        // Тип результата всегда должен быть строкой.
-        return value.toString();
     }
 }

9b1bb529037b604b8477c120d39e54267f694fe0
Alina Tarasova <alishka.tarasova@gmail.com>
2023-12-07 13:07:32 +0300

    Updated DifferTest.java

--- app/src/test/java/hexlet/code/DifferTest.java
+++ app/src/test/java/hexlet/code/DifferTest.java
@@ -1,6 +1,6 @@
 package hexlet.code;
 
-import org.junit.jupiter.api.Assertions;
+//import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
 

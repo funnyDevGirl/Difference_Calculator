f44b9546abc52977955b5285cc991ade193fbdd6
alina tarasova <alishka.tarasova@gmail.com>
2024-05-02 20:06:04 +0300

    Updated App.java

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -6,9 +6,13 @@ import picocli.CommandLine.Option;
 import picocli.CommandLine.Parameters;
 import java.util.concurrent.Callable;
 
+
 @Command(name = "gendiff", mixinStandardHelpOptions = true, version = "gendiff 4.7.5",
         description = "Compares two configuration files and shows a difference.")
-public class App implements Callable<Integer> {
+public final class App implements Callable<Integer> {
+
+    private static final int SUCCESS_EXIT_CODE = 0;
+    private static final int ERROR_EXIT_CODE = 1;
 
     @Parameters(index = "0", description = "path to first file")
     //@Parameters(description = "path to first file")
@@ -22,13 +26,20 @@ public class App implements Callable<Integer> {
 
     /**
      * @return returns a string of differences between 2 files
-     * @throws Exception  if files not exist
      */
     @Override
-    public Integer call() throws Exception {
-        String file =  Differ.generate(filepath1, filepath2, format);
-        System.out.println(file);
-        return 0;
+    public Integer call() {
+
+        try {
+            String formattedDiff =  Differ.generate(filepath1, filepath2, format);
+            System.out.println(formattedDiff);
+
+        } catch (Exception e) {
+            System.err.println(e.getMessage());
+            return ERROR_EXIT_CODE;
+        }
+
+        return SUCCESS_EXIT_CODE;
     }
 
     public static void main(String... args) {

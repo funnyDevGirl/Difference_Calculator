d9b6872d71b496469f401cc038921fd0d3d0399e
alina tarasova <alishka.tarasova@gmail.com>
2024-05-02 20:47:08 +0300

    Updated build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,6 +6,8 @@ plugins {
 
     id("se.patrikerdes.use-latest-versions") version "0.2.18"
     id("com.github.ben-manes.versions") version "0.48.0"
+
+    id("io.freefair.lombok") version "8.4"
 }
 
 group = "hexlet.code"

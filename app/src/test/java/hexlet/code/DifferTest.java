728292fda48013c204bddebc27503fa4f974aba0
alina tarasova <alishka.tarasova@gmail.com>
2024-05-03 13:27:37 +0300

    Updated DifferTest.java

--- app/src/test/java/hexlet/code/DifferTest.java
+++ app/src/test/java/hexlet/code/DifferTest.java
@@ -1,8 +1,8 @@
 package hexlet.code;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 import static org.assertj.core.api.Assertions.assertThat;
-
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -10,59 +10,27 @@ import java.nio.file.Paths;
 
 public class DifferTest {
 
-    @Test
-    public void differStylishTest1() throws Exception {
-        String filepath4 = "src/test/resources/filepath4.json";
-        String filepath5 = "src/test/resources/filepath5.json";
-        String format = "stylish";
+    @ParameterizedTest
+    @ValueSource(strings = {"json", "yml"})
+    public void generateTest(String format) throws Exception {
 
-        String expected = readFixture("filepath6.json");
-        String actual = Differ.generate(filepath4, filepath5, format);
-        assertThat(actual).isEqualTo(expected);
+        String filePath1 = getFixturePath("file1." + format).toString();
+        String filePath2 = getFixturePath("file2." + format).toString();
+
+        // Тестируем вызов метода с каждым из фоматерров,
+        // а также вызов с форматером по умолчанию
+        assertThat(Differ.generate(filePath1, filePath2))
+                .isEqualTo(readFixture("stylish.json"));
+
+        assertThat(Differ.generate(filePath1, filePath2, "json"))
+                .isEqualTo(readFixture("json.json"));
+
+        assertThat(Differ.generate(filePath1, filePath2, "stylish"))
+                .isEqualTo(readFixture("stylish.json"));
+
+        assertThat(Differ.generate(filePath1, filePath2, "plain"))
+                .isEqualTo(readFixture("plain.json"));
     }
-
-    @Test
-    public void differStylishTest2() throws Exception {
-        String filepath4 = "src/test/resources/filepath4.json";
-        String filepath5 = "src/test/resources/filepath5.json";
-
-        String expected = readFixture("filepath6.json");
-        String actual = Differ.generate(filepath4, filepath5);
-        assertThat(actual).isEqualTo(expected);
-    }
-
-    @Test
-    public void differStylishTest3() throws Exception {
-        String filepath4 = "src/test/resources/filepath1.yml";
-        String filepath5 = "src/test/resources/filepath2.yml";
-
-        String expected = readFixture("filepath3.json");
-        String actual = Differ.generate(filepath4, filepath5);
-        assertThat(actual).isEqualTo(expected);
-    }
-
-    @Test
-    public void differPlainTest() throws Exception {
-        String filepath4 = "src/test/resources/filepath4.json";
-        String filepath5 = "src/test/resources/filepath5.json";
-        String format = "plain";
-
-        String expected = readFixture("filepath7.json");
-        String actual = Differ.generate(filepath4, filepath5, format);
-        assertThat(actual).isEqualTo(expected);
-    }
-
-    @Test
-    public void differJsonTest() throws Exception {
-        String filepath4 = "src/test/resources/filepath4.json";
-        String filepath5 = "src/test/resources/filepath5.json";
-        String format = "json";
-
-        String expected = readFixture("filepath8.json");
-        String actual = Differ.generate(filepath4, filepath5, format);
-        assertThat(actual).isEqualTo(expected);
-    }
-
 
     private static Path getFixturePath(String fileName) {
         return Paths.get("src", "test", "resources", fileName)
@@ -71,7 +39,6 @@ public class DifferTest {
 
     private static String readFixture(String fileName) throws Exception {
         Path filePath = getFixturePath(fileName);
-        String result = Files.readString(filePath);
-        return result;
+        return Files.readString(filePath);
     }
 }

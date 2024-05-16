2fdff8be37e672d097b71a75bb51f1a101403cf7
alina tarasova <alishka.tarasova@gmail.com>
2024-05-16 17:04:50 +0300

    Updated Formatter.java

--- app/src/main/java/hexlet/code/Formatter.java
+++ app/src/main/java/hexlet/code/Formatter.java
@@ -3,21 +3,18 @@ package hexlet.code;
 import hexlet.code.formatters.JsonFormatter;
 import hexlet.code.formatters.PlainFormatter;
 import hexlet.code.formatters.StylishFormatter;
-
-import java.util.List;
 import java.util.Map;
+
 
 public class Formatter {
 
-    public static String generateJson(List<Map<String, Object>> differences) throws Exception {
-        return JsonFormatter.render(differences);
-    }
+    public static String format(Map<String, Object> differences, String formatName) throws Exception {
 
-    public static String generatePlain(List<Map<String, Object>> differences) throws Exception {
-        return PlainFormatter.render(differences);
-    }
-
-    public static String generateStylish(List<Map<String, Object>> differences) throws Exception {
-        return StylishFormatter.render(differences);
+        return switch (formatName) {
+            case "json" -> JsonFormatter.render(differences);
+            case "stylish" -> StylishFormatter.render(differences);
+            case "plain" -> PlainFormatter.render(differences);
+            default -> throw new Exception("Unknown format: '" + formatName + "'");
+        };
     }
 }

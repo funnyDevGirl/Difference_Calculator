ea07dda902c5f24a4649e50b8f28fc917e8adfcb
alina tarasova <alishka.tarasova@gmail.com>
2024-05-16 16:50:49 +0300

    Updated JsonFormatter.java

--- app/src/main/java/hexlet/code/formatters/JsonFormatter.java
+++ app/src/main/java/hexlet/code/formatters/JsonFormatter.java
@@ -1,13 +1,12 @@
 package hexlet.code.formatters;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-
-import java.util.List;
 import java.util.Map;
+
 
 public class JsonFormatter {
 
-    public static String render(List<Map<String, Object>> differences) throws Exception {
+    public static String render(Map<String, Object> differences) throws Exception {
 
         ObjectMapper mapper = new ObjectMapper();
         return mapper.writeValueAsString(differences);
